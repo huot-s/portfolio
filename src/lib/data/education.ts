042973280db510fcfc5612c419c1a98a1d04095f
huot-s <huot.s@free.fr>
2024-11-08 17:55:19 +0100

    finalize education part

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -4,29 +4,41 @@ import type { Education } from '../types';
 export const items: Array<Education> = [
 	{
 		degree: 'MSc Advanced Computing (with Distinction)',
-		description: 'test2',
 		location: 'London',
 		logo: Assets.ImperialCollege,
-		name: 'test',
 		organization: 'Imperial College London',
 		period: { from: new Date(2019, 9, 30), to: new Date(2020, 8, 30) },
-		shortDescription: '',
 		slug: 'imperial',
-		subjects: ['Machine Learning', 'Deep Learning', 'Natural Language Processing', 'Computer Vision', 'Finance', 'Blockchain', 'Python']
+		subjects: ['Machine Learning', 'Deep Learning', 'Natural Language Processing', 'Computer Vision', 'Finance', 'Blockchain', 'Python'],
+		description: '',
+		name: '',
+		shortDescription: '',
 	},
 	{
 		degree: 'Master (Diplôme Ingénieur)',
-		description: '',
 		location: 'Gif-sur-Yvette (91)',
 		logo: Assets.CentraleSupelec,
-		name: '',
 		organization: 'CentraleSupélec',
 		period: { from: new Date(2016, 9, 1), to: new Date(2019, 8, 30) },
-		shortDescription: '',
 		slug: 'centralesupelec',
 		subjects: ['Signal Processing', 'Mathematics', 'Statistics', 'Software engineering', 'Electronics', 'Physics',
 			'Management', 'Accountability', 'Economics', 'Corporate law', 'Project management'
-		]
+		],
+		description: '',
+		name: '',
+		shortDescription: '',
+	},
+	{
+		degree: 'CPGE (MP*)',
+		location: 'Versailles (78)',
+		logo: Assets.LyceeHoche,
+		organization: 'Lycée Hoche',
+		period: { from: new Date(2014, 9, 1), to: new Date(2016, 8, 30) },
+		slug: 'hoche',
+		subjects: ['Mathematics', 'Physics', 'Computer Science'],
+		description: '',
+		name: '',
+		shortDescription: '',
 	}
 ];
 
