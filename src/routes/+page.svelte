a7a476a648aa1ee605423b4827c2cf86706aa974
huot-s <huot.s@free.fr>
2024-11-09 17:00:01 +0100

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,7 +26,9 @@
 <div
 	class="col self-center flex-1 md:flex-row md:slef-stretch justify-center lg:justify-between items-center p-y-0px p-x-10px"
 >
-	<img class="w-120px h-120px aspect-square" src={getAssetURL(Assets.AWS)} />
+	<div class="box-content w-150px p-15px col-center">
+		<img class="w-120px h-120px aspect-square" src={getAssetURL(Assets.AWS)} />
+	</div>
 	<div class="md:flex-1 gap-10px">
 		<MainTitle classes="md:text-left ">{name} {lastName}</MainTitle>
 		<p class="text-[var(--tertiary-text)]  text-center md:text-left text-[1.2em] font-extralight">
