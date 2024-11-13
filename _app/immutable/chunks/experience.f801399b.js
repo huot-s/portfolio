c6c871a868541e9f8b03a7b97838080a8a1f72c0
huot-s <40696917+huot-s@users.noreply.github.com>
2024-11-13 14:42:37 +0000

    Deploying to gh-pages from @ huot-s/portfolio@745f5ec90bdf13f52d3ec581797760d44d4e389c 🚀

---
+++ _app/immutable/chunks/experience.f801399b.js
@@ -0,0 +1,83 @@
+import{A as e}from"./assets.92dac2c8.js";import{g as n}from"./skills.b813c7ad.js";import{C as i}from"./types.1ebf2f37.js";const a=`# The company
+---
+
+Shift Technology is a tech company that specializes in **providing artificial intelligence (AI) and machine learning solutions for the insurance industry**. Founded in 2014 and headquartered in Paris, France, Shift Technology focuses on helping insurers improve their claims processing and fraud detection capabilities.
+
+The company's flagship product is an **AI-driven platform that analyzes claims data to identify patterns and anomalies, enabling insurers to detect fraudulent activities** more effectively and streamline their claims management processes. By leveraging advanced algorithms and data analytics, Shift Technology aims to enhance operational efficiency, reduce costs, and improve the overall customer experience for insurance providers.
+
+<br><br>
+
+# Key Missions
+---
+
+- Develop & optimize an insurance fraud detection Saas solution in **C#** and **SQL**.
+- Collaborate within a **CI/CD development pipeline** under Git, GitHub, TeamCity, and Octopus.
+- Maintain a robust data pipeline that efficiently runs fraud analysis on millions of objects daily.
+- Train a fraud detection & scoring model with **Machine Learning** (LightGBM) on very imbalanced datasets.
+- **Engage directly with clients** from the sale to run phases (demo, business case, KPI workshops, governance).
+- Lead the DS Belgian team with a portfolio of four major insurance companies and over €1M annual revenue.`,t=`# The company
+---
+
+Klee Group is a company that specializes in digital transformation and IT services, focusing on providing innovative solutions to enhance business processes and improve operational efficiency. Founded in France, Klee Group offers a range of services including **software development, cloud computing, data management, and cybersecurity**. The company is known for its expertise in integrating new technologies into existing systems, helping organizations adapt to the rapidly changing digital landscape.
+
+<br><br>
+
+# Key Missions
+---
+
+- Lead the development of the **OpenData RNCS project for the INPI** under Java 8, Oracle, and Elastic Search.
+- Implement a **robust development pipeline** with SVN and Jenkins.
+- Ensure code quality through SonarQube and efficiently manage client issues via Mantis Bug Tracker.
+- Design an intuitive mobile app for a physical archive company, improve user experience with thoughtful layouts, break the project into actionable tasks with clear features, testing requirements, and timelines.`,s=`# The company
+---
+
+TNP Consultants is a **consulting firm that specializes in providing strategic and operational support to businesses** across various industries. They typically focus on areas such as management consulting, technology solutions, and business transformation. TNP Consultants may offer services like project management, process optimization, IT consulting, and change management to help organizations improve efficiency, drive innovation, and achieve their goals.
+
+<br><br>
+
+# Key Missions
+---
+
+- Setup a data pipeline and **train a pricing model** with Dataiku and Python for a multinational steel company.
+- **Deploy Machine Learning models** on AWS SageMaker and integrate them with a user-friendly POC interface.
+- Design a Power BI dashboard to provide actionable insights into a major European bank's internal operations.
+- Conceive & draft an RFP for the French government, POC for automated license plate recognition technology.
+`,o=`# The company
+---
+
+Natixis is a French corporate and investment bank that is part of the Groupe BPCE, one of the largest banking groups in France. Established in 2006, Natixis provides a wide range of financial services, including investment banking, asset management, insurance, and financial services for corporate clients and institutional investors:
+
+1. **Corporate and Investment Banking**: Natixis offers advisory services, capital markets solutions, and financing options to corporations, financial institutions, and public sector entities.
+
+2. **Asset Management**: Natixis provides investment solutions across various asset classes, including equities, fixed income, and alternative investments. It serves both institutional and retail clients.
+
+3. **Insurance**: Natixis offers a range of insurance products, including life insurance, property and casualty insurance, and employee benefits solutions.
+
+4. **Sustainable Finance**: The company is committed to sustainable finance and responsible investment, integrating environmental, social, and governance (ESG) criteria into its business practices and investment strategies.
+
+<br><br>
+
+# Key Missions
+---
+
+- **Audit Natixis applications for compliance with banking security standards** using Qualys, Checkmarx, and Zap.
+- Evaluate critical Cloud projects to address risks and enhance cloud security posture.
+- Develop dashboards KPIs with Power BI, providing insights for top management.
+- **Train developers** to integrate security best practices into the software development lifecycle`,r=`# The company
+---
+
+Airbus Defence & Space is a division of Airbus Group, focused on providing advanced aerospace and defense solutions. Established in 2014 through the merger of Airbus Military, Airbus Space, and Cassidian, the division is headquartered in Netherlands, and operates globally. The company specializes in a wide range of products and services:
+
+1. **Military Aircraft**: Airbus Defence and Space designs and manufactures military transport and aerial refueling aircraft, such as the A400M Atlas and the A330 MRTT.
+2. **Satellites**: The company is a leading provider of satellite systems for telecommunications, Earth observation, and scientific missions, including the development of the Eurostar satellite platform.
+3. **Space Exploration**: Airbus is involved in various space exploration missions, contributing to projects like the Mars Rover and the European Space Agency's (ESA) initiatives.
+4. **Defense Systems**: The division provides integrated defense solutions, including unmanned aerial vehicles (UAVs), surveillance systems, and cybersecurity services.
+
+<br><br>
+
+# Key Missions
+---
+
+- Design and **develop flight simulation software in C++**, enhancing the accuracy of flight scenarios.
+- Analyze and compute **performance metrics on existing models through Python**.
+`,u=[{slug:"ds-shift",company:"Shift Technology",name:"Senior Data and Software Engineer",contract:i.FullTime,location:"Paris",period:{from:new Date(2021,12-1,1)},type:"Software and Data Engineering",skills:n("csharp","sql","teamcity","python","git","github","octopus","rider"),color:"#ffffff00",links:[{to:"https://www.shift-technology.com",label:"Website",newTab:!0}],logo:e.ShiftTechnology,shortDescription:"Developing a fraud detection SaaS software for insurance industry.",description:a},{slug:"dev-klee",company:"Klee Group",name:"Software Engineer Consultant",contract:i.FullTime,location:"Le-Plessis-Robinson (92)",period:{from:new Date(2020,9-1,1),to:new Date(2021,11-1,30)},type:"Software Development",skills:n("java","oracle","jenkins","tortoise-svn","mantis-bt","sonarqube","eclipse"),color:"#ffffff00",links:[{to:"https://www.kleegroup.com",label:"Website",newTab:!0}],logo:e.KleeGroup,shortDescription:"Software engineer consultant operating on OpenData and mobile app projects.",description:t},{slug:"consultant-tnp",company:"TNP Consultants",name:"Junior Data Scientist Consultant",contract:i.FullTime,location:"Luxembourg",period:{from:new Date(2019,3-1,1),to:new Date(2019,8-1,30)},type:"Data Science, Consulting",skills:n("python","aws","powerbi","js","dataiku"),color:"#ffffff00",links:[{to:"https://www.tnpconsultants.com",label:"Website",newTab:!0}],logo:e.TnpConsultants,shortDescription:"Data scientist junior consultant within a rapidly growing subsidiary (Luxembourg) of a consulting firm.",description:s},{slug:"sec-natixis",company:"Natixis",name:"Cybersecurity Architect and Expert",contract:i.FullTime,location:"Paris",period:{from:new Date(2018,9-1,1),to:new Date(2019,2-1,28)},type:"Cybersecurity",skills:n("powerbi","qualys","zap","checkmarx"),color:"#ffffff00",links:[{to:"https://natixis.groupebpce.com",label:"Website",newTab:!0}],logo:e.Natixis,shortDescription:"Audit company applications and assess cloud projects within the cybersecurity team.",description:o},{slug:"intern-airbus",company:"Airbus Defence & Space",name:"Software Engineer Intern",contract:i.Internship,location:"Elancourt (78)",period:{from:new Date(2017,6-1,1),to:new Date(2017,7-1,30)},type:"Software Development",skills:n("cpp","python"),color:"#ffffff00",links:[{to:"https://www.airbus.com",label:"Website",newTab:!0}],logo:e.Airbus,shortDescription:"Internship in the Software Engineering team at Military Aircraft department.",description:r}],d="Experience";export{u as i,d as t};
