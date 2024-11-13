745f5ec90bdf13f52d3ec581797760d44d4e389c
huot-s <huot.s@free.fr>
2024-11-13 15:41:54 +0100

    update resume page

--- src/lib/data/resume.ts
+++ src/lib/data/resume.ts
@@ -1,3 +1,4 @@
 export const data = 'resume.pdf';
+export const contact = 'sebastien.huot@protonmail.com'
 
 export const title = 'Resumé';
--- src/routes/resume/+page.svelte
+++ src/routes/resume/+page.svelte
@@ -1,15 +1,36 @@
 <script>
-	import { data, title } from '@data/resume';
+	import { data, title, contact } from '@data/resume';
+	import { getAssetURL } from '$lib/data/assets';
+	import Assets from '@data/assets';
 
 	import Chip from '$lib/components/Chip/Chip.svelte';
 	import CommonPage from '$lib/components/CommonPage.svelte';
+	import CardLogo from '$lib/components/Card/CardLogo.svelte';
+
 </script>
 
 <CommonPage {title}>
 	<div class="resume">
+		{#if contact}
+			<a href={`${'mailto:'}${contact}`}>
+				<Chip size={'1.25em'}>
+					<span class="text-[1.2em]">Get in touch</span>
+				</Chip>
+			</a>
+		{/if}
+		<br><br>
 		{#if data}
 			<a href={data}>
-				<Chip size={'1.25em'}>Download Resumé (PDF)</Chip>
+				<Chip size={'1.25em'}>
+					<CardLogo
+						src={getAssetURL(Assets.PDF)}
+						alt="download"
+						radius={'0px'}
+						size={20}
+						classes="mr-2"
+					/>
+					<span class="text-[1.2em]">Download Resumé (PDF)</span>
+				</Chip>
 			</a>
 		{:else}
 			<Chip>Ooops! no CV at the moment.</Chip>
@@ -19,8 +40,8 @@
 
 <style lang="scss">
 	.resume {
-		display: flex;
-		justify-content: center;
+		display: inline;
+		text-align: center;
 		margin-top: 20px;
 
 		& > a {
